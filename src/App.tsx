5f91efdca328eed4b109cb86503362209543453d
Rodrigo <ro@magno.mx>
2025-09-26 16:21:12 -0600

    Agregar ruta con barra final para /policies/privacy/

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/policies/privacy" element={<Privacy />} />
+          <Route path="/policies/privacy/" element={<Privacy />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
